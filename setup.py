37a97c712de03f1c00f1d4aecd817510512aa3b9
Tautvydas Petkus <tauptk@telesoftas.com>
2020-02-25 17:01:49 +0200

    Backport to Python 2.7
    Module does not use exclusive Python 3 functionality, so the module was backported to be supported by Python 2.7
    
    Typing was adjusted with additional dependency, which is available on Python 2.7 via pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="cancel_token",
-    version="0.1.2",
+    version="0.1.3",
     author="Maciej Nowak",
     description="Simple CancellationToken",
     long_description=long_description,
@@ -13,6 +13,7 @@ setup(
     url="https://github.com/Novakov/py-cancel-token",
     packages=['cancel_token'],
     classifiers=[
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
@@ -20,6 +21,5 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Utilities"
     ],
-    python_requires='>=3.7',
     include_package_data=True
 )
